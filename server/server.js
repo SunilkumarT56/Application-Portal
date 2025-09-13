726b31d6eba5b927f71cf1fae6b29dfd08aaa9fa
sunilkumar <sunilkumar@sunilkumars-MacBook-Air.local>
2025-09-13 13:55:18 +0530

    added clerk webhook in backend for 3 events

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ import "dotenv/config";
 import connedtDB from "./config/db.js";
 const PORT = process.env.PORT;
 const app = express();
-import "./config/instrument.js"
-import * as Sentry from "@sentry/node"
+import "./config/instrument.js";
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 await connedtDB();
 
 //Middlewares
@@ -20,16 +21,16 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 
-
+app.post("/webhooks", clerkWebhooks);
 
 Sentry.setupExpressErrorHandler(app);
-
 app.use(function onError(err, req, res, next) {
   // The error id is attached to `res.sentry` to be returned
   // and optionally displayed to the user for support.
   res.statusCode = 500;
   res.end(res.sentry + "\n");
 });
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on Port ${PORT}`);
 });
