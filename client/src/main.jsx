ab76110939e14be85a1a81058d92de8fe8f568a4
RATISH S <saravanankavitha876@gmail.com>
2025-09-15 00:16:40 +0530

    Fronted checked its working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import './index.css'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AppContextProvider } from './context/AppContext'; 
+import {ClerkProvider} from '@clerk/clerk-react'
+
+
+const PUBLISHABLE_KEY = "pk_test_dG9sZXJhbnQtcGVuZ3Vpbi0xNS5jbGVyay5hY2NvdW50cy5kZXYk"
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
       <AppContextProvider>
         <App />
       </AppContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </ClerkProvider>,
 );
 
