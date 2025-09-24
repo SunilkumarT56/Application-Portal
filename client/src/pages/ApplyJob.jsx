956b4ed42a8559c66c7f6f2c74630ad444a4b542
RATISH S <saravanankavitha876@gmail.com>
2025-09-24 16:49:00 +0530

    Added some

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { assets, jobsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import Navbar from '../components/Navbar'
 
 const ApplyJob = () => {
-  return (
-    <div>
-      <h1 className='text-gray-600'>Hello world</h1>
-    </div>
+
+  const { id } = useParams()
+
+  const [jobData, setJobData] = useState(null)
+
+  const { jobs } = useContext(AppContext)
+
+  const fetchJob = async () => {
+    const data = jobs.filter(job => job._id === id)
+    if (data.length !== 0) {
+      setJobData(data[0])
+      console.log(data[0])
+    }
+  }
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJob()
+    }
+
+  }, [id, jobs])
+
+  return jobData ? (
+    <>
+      <Navbar/>
+      <div>
+        <div>
+          <div>
+            <div>
+              <img src={assets.company_icon} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <Loading/>
   )
 }
 
